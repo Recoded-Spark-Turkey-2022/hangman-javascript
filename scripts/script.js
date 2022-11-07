cf12647202210c88cf3aa0754fdaf605ecf557ff
yasincandev <canyasin1996@gmail.com>
2022-11-07 15:04:38 +0300

    basic functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,43 @@
-fetch("https://random-word-api.herokuapp.com/word?number=1")
-  .then((response) => response.json())
-  .then((data) => {
-    const word = data[0];
-  });
+let alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
 
-const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
-
-const buttons = document.querySelector(".buttons");
+const buttonContainer = document.querySelector(".buttons");
 
 for (let i = 0; i < alphabet.length; i++) {
   const button = document.createElement("button");
   button.textContent = alphabet[i];
   button.classList.add("btn");
-  buttons.appendChild(button);
+  buttonContainer.appendChild(button);
 }
+
+let word = "";
+
+fetch("https://random-word-api.herokuapp.com/word?number=1")
+  .then((response) => response.json())
+  .then((data) => {
+    word = data[0];
+    console.log(word);
+    const wordContainer = document.querySelector(".word");
+    for (let i = 0; i < word.length; i++) {
+      const letter = document.createElement("span");
+      letter.textContent = "_";
+      letter.classList.add("letter");
+
+      wordContainer.appendChild(letter);
+    }
+  });
+
+const checkLetter = (e) => {
+  const letter = e.target.textContent;
+  const letters = document.querySelectorAll(".letter");
+
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === letter) {
+      letters[i].textContent = letter;
+    }
+  }
+};
+
+const buttons = document.querySelectorAll(".btn");
+buttons.forEach((button) => {
+  button.addEventListener("click", checkLetter);
+});
